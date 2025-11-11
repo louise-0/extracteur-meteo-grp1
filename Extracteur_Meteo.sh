8b85fc40c3077c414b2adc42f7d27abb08cfa446
Faycalbhmd44 <faycal.baahmed@univ-tlse3.fr>
2025-11-11 18:13:29 +0100

    Ajout de la question 3 : Formatter les informations récupérées

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -20,3 +20,15 @@ echo "meteo enregistree dans $Fichier"
 git add Extracteur_Meteo.sh
 git commit -m " ajout de la partie 2 pour récupération météo"
 git push origin version1
+
+TempActuelle=$(curl -s "https://wttr.in/${Ville}?format=%t")
+TempDemain=$(curl -s "https://wttr.in/${Ville}?1" | grep -oE '[+-]?[0-9]+°C' | sed -n '2p')
+if [ -z "$TempDemain" ]; then
+    TempDemain="N/A"
+fi
+Jour=$(date +"%Y-%m-%d")
+Heure=$(date +"%H:%M")
+Ligne="${Jour} - ${Heure} - ${Ville} : ${TempActuelle} - ${TempDemain}"
+echo "$Ligne" >> meteo.txt
+echo "Données formatées enregistrées dans meteo.txt"
+
